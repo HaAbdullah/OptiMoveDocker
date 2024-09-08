1cd011ca48c1aecab33f517f3c10ca4a50c76943
HaAbdullah <abdullah.hasanjee@gmail.com>
2024-09-08 02:16:51 -0400

    Removed duplicate false copies and confirmed 4 test cases per image

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from FENtoMove import FENtoMove
 import os
 
 # Ask the user for viewing side and active color
-user_input_side = "WHITE"
-user_input_color = "w"  # or "b" for black's turn
+user_viewing_side = "BLACK"
+user_turn_color = "b"  # or "b" for black's turn
 
-board = nameAllTiles(user_input_side, r"Inputs/input1.png")
+board = nameAllTiles(user_viewing_side, r"Inputs/input1.png")
 printAllTiles(board)
-fen = convert_to_FEN(board, user_input_color)
+fen = convert_to_FEN(board, user_turn_color)
 move = FENtoMove(fen)
 
 print(f"FEN: {fen}")

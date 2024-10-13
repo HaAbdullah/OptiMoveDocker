95a1b124c580fb3d7050cdf8adb4a6ab93132df4
HaAbdullah <abdullah.hasanjee@gmail.com>
2024-10-12 21:16:47 -0400

    user input functionality added to any image in current directory

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ import os
 # Ask the user for viewing side and active color
 user_viewing_side = "BLACK" # BLACK or WHITE
 user_turn_color = "b"  # or "b" for black's turn
+folder_path = "."
+image_files = [f for f in os.listdir(folder_path) if f.endswith(('.png', '.jpg', '.jpeg'))]
 
-board = nameAllTiles(user_viewing_side, r"img/input3.png")
-printAllTiles(board)
-fen = convert_to_FEN(board, user_turn_color)
-move = FENtoMove(fen)
+if len(image_files) == 1:
+    image_path = os.path.join(folder_path, image_files[0])
+    board = nameAllTiles(user_viewing_side, image_path)
+    printAllTiles(board)
+    fen = convert_to_FEN(board, user_turn_color)
+    move = FENtoMove(fen)
+    print(f"FEN: {fen}")
+    print("The best move is: " + move)
+else:
+    print("Please ensure there is exactly one image file in the folder.")
+    
 
-print(f"FEN: {fen}")
-print("The best move is: " + move)
+# board = nameAllTiles(user_viewing_side, r"img/input3.png")
+# printAllTiles(board)
+# fen = convert_to_FEN(board, user_turn_color)
+# move = FENtoMove(fen)
+
+# print(f"FEN: {fen}")
+# print("The best move is: " + move)
 
